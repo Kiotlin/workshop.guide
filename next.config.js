eeac65582c15a6a80c3e79b3485a1b577d52c7ac
ri1ken <719401217@qq.com>
2022-05-23 02:10:26 +0900

    Append new contents.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_staticImage: true,
+  unstable_staticImage: true
 })
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    domains:[
+      'pbs.twimg.com'
+    ],
+  }
+})
